835456a26253247a96cd127cd27cf00c28d78cd2
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-01-24 13:42:55 +0100

    changed Link to NavLink for easier controll,

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar () {
   return (
     <nav>
       <ul>
-        <li><Link to='/dashboard/applications'>Applications</Link></li>
-        <li><Link to='/dashboard/events'>Events</Link></li>
-        <li><Link to='/dashboard/profile'>Profille</Link></li>
-        <li><Link to='/logout'>Log Out</Link></li>
+        <li><NavLink to='/dashboard/applications' >Applications</NavLink></li>
+        <li><NavLink to='/dashboard/events'>Events</NavLink></li>
+        <li><NavLink to='/dashboard/profile'>Profille</NavLink></li>
+        <li><NavLink to='/logout'>Log Out</NavLink></li>
       </ul>
     </nav>
   )
