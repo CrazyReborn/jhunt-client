898e364b76bf8311b7e78abff747d143862cf7d1
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-01-24 19:06:21 +0100

    added a simple version of the dashboard page.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,16 +3,19 @@ import Navbar from './Navbar';
 import Applications from './Applications';
 import Events from "./Events";
 import Profile from "./Profile";
+import '../styles/MainPage.css';
 
 export default function MainPage () {
   return (
     <div className="mainContent">
       <Navbar />
-      <Routes>
-        <Route path='/applications' element={<Applications />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/profile' element={<Profile />} />
+      <div className="outlet">
+        <Routes>
+          <Route path='/applications' element={<Applications />} />
+          <Route path='/events' element={<Events />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
+        </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import '../styles/Navbar.css';
 
 export default function Navbar () {
   return (
@@ -6,6 +7,7 @@ export default function Navbar () {
       <ul>
         <li><NavLink to='/dashboard/applications' >Applications</NavLink></li>
         <li><NavLink to='/dashboard/events'>Events</NavLink></li>
+        <li><NavLink to='/dashboard/interviews'>Interviews</NavLink></li>
         <li><NavLink to='/dashboard/profile'>Profille</NavLink></li>
         <li><NavLink to='/logout'>Log Out</NavLink></li>
       </ul>
