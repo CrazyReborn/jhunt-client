4bf5a5663c100b8749278d6d0ebede67a903d017
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-06-27 16:24:59 +0200

    added a mobile version of the navbar. made the navbars responsive.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,18 @@ import { NavLink } from 'react-router-dom';
 import { ReactComponent as ApplicationsLogo } from '../images/description_black_24dp.svg';
 import { ReactComponent as ProfileLogo } from '../images/manage_accounts_black_24dp.svg';
 import { ReactComponent as LogoutLogo } from '../images/logout_black_24dp.svg';
+import { ReactComponent as MenuLogo } from '../images/bars-solid.svg';
+import { ReactComponent as CloseLogo } from '../images/close_black_24dp.svg';
 import '../styles/Navbar.css';
 import NewApplicationForm from './Applications/NewApplicationForm';
 
 // eslint-disable-next-line react/prop-types
 export default function Navbar({ rerender, setRerender }) {
   const [creatingNew, setCreatingNew] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
   return (
     <>
-      <nav>
+      <nav className="desktop-nav">
         <button type="button" className="btn-action" onClick={() => setCreatingNew(true)}>Add New Application</button>
         <ul>
           <li>
@@ -33,6 +36,36 @@ export default function Navbar({ rerender, setRerender }) {
             </NavLink>
           </li>
         </ul>
+      </nav>
+      <MenuLogo className={`mobile-bars ${openMenu ? 'opened' : ''}`} onClick={() => setOpenMenu(true)} />
+      <nav className={`mobile-nav ${openMenu ? 'opened' : null}`}>
+        {openMenu
+          && (
+            <>
+              <CloseLogo className="mobile-menu-close" onClick={() => setOpenMenu(false)} />
+              <button type="button" className="btn-action" onClick={() => { setOpenMenu(false); setCreatingNew(true); }}>Add New Application</button>
+              <ul>
+                <li>
+                  <NavLink onClick={() => setOpenMenu(false)} to="/dashboard/applications">
+                    <ApplicationsLogo fill="blue" />
+                    Applications
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink onClick={() => setOpenMenu(false)} to="/dashboard/profile">
+                    <ProfileLogo fill="blue" />
+                    Profille
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink onClick={() => setOpenMenu(false)} to="/logout">
+                    <LogoutLogo fill="blue" />
+                    Log Out
+                  </NavLink>
+                </li>
+              </ul>
+            </>
+          )}
       </nav>
       <NewApplicationForm
         creatingNew={creatingNew}
